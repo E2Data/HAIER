3adf87d65a0217a92ebedec65c540b72248e8aa9
Christos Katsakioris <ckatsak@gmail.com>
2019-08-30 12:31:52 +0000

    Add multipart/form-data endpoint for file upload

--- src/main/java/gr/ntua/ece/cslab/e2datascheduler/ws/SchedulerService.java
+++ src/main/java/gr/ntua/ece/cslab/e2datascheduler/ws/SchedulerService.java
@@ -7,14 +7,23 @@ import gr.ntua.ece.cslab.e2datascheduler.beans.optpolicy.OptimizationPolicy;
 import gr.ntua.ece.cslab.e2datascheduler.beans.graph.ExecutionGraph;
 import gr.ntua.ece.cslab.e2datascheduler.beans.graph.ToyJobGraph;
 
+import org.glassfish.jersey.media.multipart.FormDataContentDisposition;
+import org.glassfish.jersey.media.multipart.FormDataParam;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import java.util.logging.Logger;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
 /**
  * REST API of {@link E2dScheduler}
@@ -25,6 +34,9 @@ import java.util.logging.Logger;
 public class SchedulerService extends AbstractE2DataService {
 
     private static final Logger logger = Logger.getLogger(SchedulerService.class.getCanonicalName());
+
+    // FIXME(ckatsak): should be configurable rather than hard-coded, with a sane default (e.g. "/tmp/")
+    private static final String tmpRootPath = "/home/users/ckatsak/haier_tmp/";
 
     private E2dScheduler scheduler;
 
@@ -61,6 +73,48 @@ public class SchedulerService extends AbstractE2DataService {
     public Response schedule() {
         String happyMesg = "hello";
         return generateResponse(Response.Status.OK, happyMesg);
+    }
+
+    @Path("/flink-schedule")
+    @POST
+    @Consumes(MediaType.MULTIPART_FORM_DATA)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response flink_schedule(
+            @FormDataParam("file") InputStream uploadedInputStream,
+            @FormDataParam("file") FormDataContentDisposition fileDetail) {
+
+        final String filePath = this.tmpRootPath + fileDetail.getFileName();
+
+        try {
+            this.storeSerializedJobGraphFile(uploadedInputStream, filePath);
+            logger.info("JobGraph file '" + fileDetail.getFileName() + "' has been uploaded successfully; stored as '" + filePath + "'.");
+        } catch (IOException e) {
+            logger.warning("Error uploading JobGraph file '" + fileDetail.getFileName() + "'");
+            e.printStackTrace();
+            return generateResponse(Response.Status.INTERNAL_SERVER_ERROR, "Error uploading JobGraph file '" + fileDetail.getFileName() + "'.");
+        }
+
+        try {
+            // TODO(ckatsak): deserialize
+        } finally {
+            // TODO(ckatsak): delete temporarily stored jobgraph file
+        }
+
+        // TODO(ckatsak): schedule
+
+        return generateResponse(Response.Status.OK, "File '" + fileDetail.getFileName() + "' has been uploaded successfully!");
+    }
+
+    private void storeSerializedJobGraphFile(final InputStream uploadedInputStream, final String filePath) throws IOException {
+        final FileOutputStream out = new FileOutputStream(new File(filePath));
+        final byte[] bytes = new byte[8192];  // FIXME(ckatsak): looks ugly
+        int read = 0;
+        while ((read = uploadedInputStream.read(bytes)) != -1) {
+            //logger.info("Just read " + read + " bytes from the file being uploaded.");
+            out.write(bytes, 0, read);
+        }
+        out.flush();
+        out.close();
     }
 
 }
