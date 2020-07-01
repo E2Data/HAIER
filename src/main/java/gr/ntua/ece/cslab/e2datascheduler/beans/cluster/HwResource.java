2e34f07ef7ce32c09b65d05cd220da27336f0202
Christos Katsakioris <ckatsak@gmail.com>
2020-07-01 20:47:57 +0300

    Fix # of HwResource instances, openapi.yml
    Cluster hardware resources of the same model are reported by Yarn as a
    single hardware resource with .value > 1.
    Now this translates to the allocation multiple HwResource instances for
    the NSGA-II optimization.
    
    Also a couple of minor improvements to openapi.yml.
    
    Signed-off-by: Christos Katsakioris <ckatsak@gmail.com>

--- src/main/java/gr/ntua/ece/cslab/e2datascheduler/beans/cluster/HwResource.java
+++ src/main/java/gr/ntua/ece/cslab/e2datascheduler/beans/cluster/HwResource.java
@@ -1,7 +1,7 @@
 package gr.ntua.ece.cslab.e2datascheduler.beans.cluster;
 
 /**
- * Generic description of a Hardware resource
+ * Generic description of a hardware resource.
  */
 public class HwResource {
 
@@ -11,6 +11,27 @@ public class HwResource {
     private String units; // has meaning only in memory and indicates whether the reported amount is counted in MB/GB etc.
     private int value;
     private String host; // indicates the machine where this resource exists.
+
+    /**
+     * Nullary constructor.
+     */
+    public HwResource() {}
+
+    /**
+     * Clone from an existing instance, but setting the {@code value} field to 1 and appending <code>-{id}</code>
+     * to its {@code name} field.
+     *
+     * @param hwResource is the existing instance.
+     * @param id is the ID to append to existing instance's {@code name} field.
+     */
+    public HwResource(final HwResource hwResource, final int id) {
+        this.maximumAllocation = hwResource.maximumAllocation;
+        this.minimumAllocation = hwResource.minimumAllocation;
+        this.name = hwResource.name + "-" + id;
+        this.units = hwResource.units;
+        this.value = 1;
+        this.host = hwResource.host;
+    }
 
     public long getMaximumAllocation() {
         return maximumAllocation;
--- src/main/java/gr/ntua/ece/cslab/e2datascheduler/optimizer/nsga/NSGAIIHaierOptimizer.java
+++ src/main/java/gr/ntua/ece/cslab/e2datascheduler/optimizer/nsga/NSGAIIHaierOptimizer.java
@@ -22,10 +22,10 @@ import java.util.logging.Logger;
 
 
 /**
- * Implementation of an E2Data HAIER's optimizer that is based on the use of the NSGAII genetic algorithm and
+ * Implementation of an E2Data HAIER's optimizer that is based on the use of the NSGA0II genetic algorithm and
  * Machine Learning models for the evaluation of each of its generations.
  */
-public class NSGAIIHaierOptimizer implements Optimizer {
+public final class NSGAIIHaierOptimizer implements Optimizer {
 
     private static final Logger logger = Logger.getLogger(NSGAIIHaierOptimizer.class.getCanonicalName());
 
@@ -41,9 +41,20 @@ public class NSGAIIHaierOptimizer implements Optimizer {
      * A list of the available {@link HwResource}s in the cluster, to assign the
      * tasks ({@link org.apache.flink.runtime.jobgraph.JobVertex} objects) on.
      */
-    private List<HwResource> devices;
+    private final List<HwResource> devices;
+
 
     // --------------------------------------------------------------------------------------------
+
+
+    /**
+     * Getter method for the list of the available {@link HwResource}s in the cluster.
+     *
+     * @return the list of available {@link HwResource}s in the cluster.
+     */
+    public synchronized List<HwResource> getDevices() {
+        return this.devices;
+    }
 
     /**
      * Implementation of an E2Data HAIER's optimizer that is based on the use of the NSGA-II genetic algorithm and
@@ -66,7 +77,7 @@ public class NSGAIIHaierOptimizer implements Optimizer {
      */
     @Override
     public synchronized Parameters retrieveConfiguration() {
-        logger.info("Retrieving this.parameters.maxParetoPlans (= " + this.parameters.getMaxParetoPlans() + ") and " +
+        logger.fine("Retrieving this.parameters.maxParetoPlans (= " + this.parameters.getMaxParetoPlans() + ") and " +
                 "this.parameters.numGenerations (= " + this.parameters.getNumGenerations() + ").");
         return this.parameters;
     }
@@ -74,75 +85,36 @@ public class NSGAIIHaierOptimizer implements Optimizer {
     /**
      * Set new values for the NSGA-II parameters in a synchronized way with respect to concurrent getters.
      *
-     * @param parameters contains the new values of the NSGA-II parameters to be set; must be an object of the
+     * @param parameters contains the new values of the NSGA-II parameters to be set; must be an instance of the
      *                   {@link NSGAIIParameters} class.
      */
     @Override
     public synchronized void configure(final Parameters parameters) {
         this.parameters = (NSGAIIParameters) parameters;
-        logger.info("Just set this.parameters.maxParetoPlans to " + this.parameters.getMaxParetoPlans() +
+        logger.fine("Just set this.parameters.maxParetoPlans to " + this.parameters.getMaxParetoPlans() +
                 " and this.parameters.numGenerations to " + this.parameters.getNumGenerations() + ".");
     }
 
-    public List<HwResource> getDevices() {
-        return this.devices;
-    }
-
-    public void setDevices(final List<HwResource> devices) {
-        this.devices = devices;
-    }
 
     // --------------------------------------------------------------------------------------------
 
 
-    // FIXME(ckatsak): When GUI is in, the NSGAIIParameters are read by E2dScheduler to allocate a BlockingQueue.
-    //                 Therefore, a (currently unhandled) race condition would occur if the NSGAIIParameters were
-    //                 changed between the allocation of the BlockingQueue in E2dScheduler.schedule() and the
-    //                 configuration of the NSGA-II Executor in here.
-    //                 Alternatively, we could use `synchronized` here; however this would effectively serialize
-    //                 the process of all scheduling requests that HAIER handles.
-    public List<HaierExecutionGraph> optimize(
+    // NOTE(ckatsak): This method has to be synchronized because multiple handler threads may run it concurrently,
+    //                thus messing with one another's available cluster hardware resources.
+    //                Therefore, the process of each scheduling request that HAIER handles is effectively serialized.
+    public synchronized List<HaierExecutionGraph> optimize(
             final JobGraph flinkJobGraph,
             final OptimizationPolicy policy,
             final Model mlModel) {
         // On each optimization request, obtain a fresh view of the available cluster resources.
-        final YarnCluster cluster = YarnCluster.getInstance();
-        if (cluster != null) {
-            devices.clear();
-
-            for (ClusterNode node : cluster.getNodes()) {
-                logger.info("Yarn reported node: " + node);
-                final ArrayList<HwResource>  availableResources = node.getAvailableResources();
-                for (HwResource r : availableResources) {
-                    logger.info("\tYarn reported resource: " + r);
-                    devices.add(r);
-                }
-            }
-        } //FIXME(gmytil): what is the expected behavior if I cannot get fresh status from the cluster?
-/*
-        //  vv  FIXME(ckatsak)  vv
-        // Yarn workaround -- hard-coding devices
-        HwResource r = new HwResource();
-        r.setHost("silver1");
-        r.setName("cpu");
-        devices.add(r);
-        r = new HwResource();
-        r.setHost("gold1");
-        r.setName("gtx");
-        devices.add(r);
-        r = new HwResource();
-        r.setHost("gold2");
-        r.setName("tesla");
-        devices.add(r);
-        //  ^^  FIXME(ckatsak)  ^^
-*/
-
-
-        // Construct the problem.
-        final NSGAIIHaierPlanning problem = new NSGAIIHaierPlanning(devices, mlModel, flinkJobGraph, policy);
+        this.retrieveAvailableHardwareResources();  // FIXME(ckatsak): Currently intolerant to Yarn failures.
+        logger.info("Cluster hardware resources available for this allocation: " + this.devices);
 
         // Lock on specific problem parameters.
         final NSGAIIParameters problemParams = (NSGAIIParameters) this.retrieveConfiguration(); // synchronized access
+
+        // Construct the problem.
+        final NSGAIIHaierPlanning problem = new NSGAIIHaierPlanning(devices, mlModel, flinkJobGraph, policy);
 
         // Run NSGA-II.
         final NondominatedPopulation result = new Executor()
@@ -152,15 +124,64 @@ public class NSGAIIHaierOptimizer implements Optimizer {
                 .withMaxEvaluations(problemParams.getNumGenerations())
                 .run();
 
-        // Delegate the final selection of the HaierExecutionGraph to return,
-        // to the provided OptimizationPolicy object.
+        // Return the list of the available HaierExecutionGraphs (i.e., execution plans).
         final List<HaierExecutionGraph> paretoHaierExecutionGraphs = new ArrayList<>(problemParams.getMaxParetoPlans());
         for (Solution solution : result) {
             paretoHaierExecutionGraphs.add(problem.solutionGraphs.get(solution));
         }
-        logger.fine("Pareto frontier (" + paretoHaierExecutionGraphs.size() + " solutions): " +
+        logger.finer("Pareto frontier (" + paretoHaierExecutionGraphs.size() + " solutions): " +
                 paretoHaierExecutionGraphs);
         return paretoHaierExecutionGraphs;
+    }
+
+    /**
+     * Obtain a fresh view of the available cluster resources from Yarn.
+     *
+     * FIXME(gmytil): what is the expected behavior if I cannot get fresh status from the cluster?
+     */
+    private void retrieveAvailableHardwareResources() {
+        final YarnCluster cluster = YarnCluster.getInstance();
+        if (null == cluster) {
+            logger.severe("Could not retrieve a fresh view of the cluster from Yarn!");
+            return;
+        }
+        logger.info("Successfully retrieved a fresh view of the cluster from Yarn.");
+
+        devices.clear();
+        for (ClusterNode node : cluster.getNodes()) {
+            logger.fine("Yarn detected cluster node: " + node);
+            final ArrayList<HwResource>  availableResources = node.getAvailableResources();
+            for (HwResource r : availableResources) {
+                logger.finer("\tYarn detected available hardware resource: " + r);
+                // If multiple GPUs or FPGAs of the same model are present, they should be represented as distinct
+                // hardware resource objects; so we allocate distinct HwResource instances for each of them.
+                if (r.getName().startsWith("yarn.io/gpu") || r.getName().startsWith("yarn.io/fpga")) {
+                    for (int i = 0; i < r.getValue(); i++) {
+                        logger.finer("\t\tCreating distinct hardware resource no" + (i + 1));
+                        devices.add(new HwResource(r, i));
+                    }
+                } else {
+                    devices.add(r);
+                }
+            }
+        }
+/*
+            //  vv  FIXME(ckatsak)  vv
+            // Yarn workaround -- hard-coding devices
+            HwResource r = new HwResource();
+            r.setHost("silver1");
+            r.setName("cpu");
+            devices.add(r);
+            r = new HwResource();
+            r.setHost("gold1");
+            r.setName("gtx");
+            devices.add(r);
+            r = new HwResource();
+            r.setHost("gold2");
+            r.setName("tesla");
+            devices.add(r);
+            //  ^^  FIXME(ckatsak)  ^^
+*/
     }
 
 }
