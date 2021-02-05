62d7e1f098fda93cefecf4c693ad8e2ad648c199
Christos Katsakioris <ckatsak@gmail.com>
2021-02-05 11:53:40 +0200

    Add HTTP headers for CORS
    Signed-off-by: Christos Katsakioris <ckatsak@gmail.com>

--- src/main/java/gr/ntua/ece/cslab/e2datascheduler/ws/AbstractE2DataService.java
+++ src/main/java/gr/ntua/ece/cslab/e2datascheduler/ws/AbstractE2DataService.java
@@ -2,11 +2,22 @@ package gr.ntua.ece.cslab.e2datascheduler.ws;
 
 import javax.ws.rs.core.Response;
 
+
 public class AbstractE2DataService {
 
-    public Response generateResponse(Response.StatusType httpStatus, Object obj) {
-        Response response = Response.status(httpStatus).entity(obj).build();
-        return response;
+    public Response generateResponse(final Response.StatusType httpStatus, final Object obj) {
+        final Response ret = Response
+                .status(httpStatus)
+                // TODO(ckatsak): Are these headers enough for CORS?
+                // FIXME(ckatsak): It would be cleaner if CORS headers were only injected for responses to
+                //                 the GUI, and obviously if they were fine-grained rather than wildcards.
+                .header("Access-Control-Allow-Origin", "*")
+                .header("Access-Control-Allow-Methods", "*")
+                .header("Access-Control-Allow-Headers", "*")
+                .header("Access-Control-Expose-Headers", "*")
+                .entity(obj)
+                .build();
+        return ret;
     }
 
 }
